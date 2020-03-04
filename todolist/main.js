1f59535150bb1936124ba4a61f044fe186617689
Olsonrowan <56852849+Olsonrowan@users.noreply.github.com>
2020-03-04 09:38:14 -0700

    added plain css/js with comments
    add css, and js structure i plan on using. may change though.

---
+++ todolist/main.js
@@ -0,0 +1,50 @@
+// class User {
+//     constructor(lists = []) {
+//         this.lists = lists;
+//     }
+//     addList(list) {
+//         this.lists.push(list);
+//     }
+// }
+
+
+// class ToDoList{
+//     constructor(id, name, toDos = []) {
+//         this.id = id;
+//         this.name = name;
+//         this.toDos = toDos;
+//     }
+
+//     rename(newName) {
+//         this.name = newName;
+    
+//     }
+
+//     addTodo(toDo){
+//         this.toDos.push(toDo);
+
+//     }
+
+//     removeToDo(id) {
+//         this.toDos = this.toDos.filter(toDo => toDo.id != id);
+
+//     }
+// }
+
+// class ToDo {
+//     constructor(id, text){
+//         this.id = id;
+//         this.text = text;
+//         this.completed = false;
+
+//     }
+//     edit(newText){
+//         this.text = newText;
+
+//     }
+//     setCompleted(completed){
+//         this.completed = completed;
+
+//     }
+
+// }
