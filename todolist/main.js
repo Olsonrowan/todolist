779cb28c8d5715c2b0dd545a909965830299490a
Olsonrowan <56852849+Olsonrowan@users.noreply.github.com>
2020-03-12 08:55:13 -0600

    function to add lists and delete them
    added function to add list items and delete them with some css/js

--- todolist/main.js
+++ todolist/main.js
@@ -48,3 +48,86 @@
 //     }
 
 // }
+
+// var listbtn = document.getElementById('btn1');
+// buttonclick.addEventListener('click', function () {
+//     append.li
+
+// });
+
+
+var input = document.querySelector("input[type = 'text']");
+var ul = document.querySelector("ul");
+var text = " "
+var list = document.getElementById("list");
+var add = document.getElementById('btn1');
+
+// add.addEventListener('click', function() {
+//   list.innerText += `<li>item</li>`
+// });
+
+
+// input.addEventListener('keypress', function(keyPressed) {
+//     if (keyPressed.which === 13) {
+//         var li = document.createElement('li');
+//          var newTodo = this.value;
+//          this.value = " " ;
+//          ul.appendChild(li).append(newTodo)
+//     }
+//   });
+
+  var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+//this is to add list item
+function myFunction() {
+  var li = document.createElement("li");
+  var inputValue = document.querySelector("input[type = 'text']").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("there must be something to do today!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("taskText").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");  //adding X for close on li
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7"); 
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
